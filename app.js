9b369e4ad0a997e77a42046b62c4d4bd4e65a83d
Abhishek Alevoor <abhishek.alevoor@logmein.com>
2018-07-26 09:12:24 +0200

    ground work completed

--- app.js
+++ app.js
@@ -2,18 +2,66 @@ var express = require('express');
 var app = express();
 
 // set up handlebars view engine
-var handlebars = require('express3-handlebars').create({ defaultLayout: 'main' });
+// var handlebars = require('express3-handlebars').create({ defaultLayout: 'main' });
 
-var fortune = require('./lib/fortune.js');
+var handlebars = require('express3-handlebars').create({defaultLayout: 'main', 
+                                                        helpers: {
+                                                            section: function(name, options) {
+                                                                if(!this._sections) this._sections = {};
+                                                                this._sections[name] = options.fn(this);
+                                                                return null;
+                                                                }
+                                                            }
+                                                       });
+
 
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+var fortune = require('./lib/fortune.js');
+
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+
+
+function getWeatherData(){
+    return {
+        locations:[
+            {
+                name: 'Portland',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Overcast',
+                temp: '54.1 F (12.3 C)',
+            },
+            {
+                name: 'Bend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Partly Cloudy',
+                temp: '55.0 F (12.8 C)',
+            },
+            {
+                name: 'Manzanita',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Overcast',
+                temp: '55.0 F (12.8 C)',
+            },
+        ],
+    };
+}
+
+
+/* app.use(function(){
+    if(!res.locals.partials) res.locals.partials = {};
+    res.locals.partials.weather = getWeatherData();
+    next();
+});
+*/
 app.use(function(req, res, next){
     res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
     next();
@@ -56,6 +104,36 @@ app.get('/headers', function(req,res) {
     for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
     res.send(s);
 });
+
+/* Response codes other than 200
+app.get('/error', function(req, res) {
+    res.status(500);
+    res.render('error');
+});
+*/
+
+/* Passing a context to a view, including querystring, cookie, and session values
+app.get('/greeting', function(req, res){
+    res.render('about', {
+        message: 'welcome',
+        style: req.query.style,
+        userid: req.cookie.userid,
+        username: req.session.username,
+    });
+});
+*/
+
+/* Rendering plaintext output */
+
+app.get('/test', function(req, res){
+    res.type('text/plain');
+    res.send('this is a test');
+});
+
+app.get('/jquery', function(req, res) {
+    res.render('jquery-test');
+});
+
 
 
 
